e0df301f457737c544be8d04dcbdae068345be74
A1wol <a1wol@protonmail.com>
2023-04-30 00:36:58 +0200

    Uber uns project pushed to GitHub

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+let modal = document.getElementById("modal");
+let img = document.getElementsByTagName('img').item(0);
+let modalImg = document.getElementById("modalImg");
+let captionText = document.getElementById("modal__header");
+document.querySelectorAll(".slide-img").forEach((item) => {
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            modal.style.display = "none";
+        }
+    });
+    item.addEventListener("click", (event) => {
+        modal.style.display = "flex";
+        modal.style.position = "fixed";
+        modalImg.src = event.target.src;
+        captionText.innerHTML = event.target.alt;
+    });
+});
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modal.style.display = "none";
+} 
